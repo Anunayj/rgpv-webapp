f9f6377e5371dc0f723cc4f5acb8a88d9a494c3a
Anunay Jain <anunay.j1@gmail.com>
2019-07-07 22:48:17 +0530

    I'm dumb

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,33 @@ const FILES_TO_CACHE = [
   'icon512.ong',
 ];
 
+    
+/*
+ * @license
+ * Your First PWA Codelab (https://g.co/codelabs/pwa)
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+'use strict';
 
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  // CODELAB: Precache static resources here.
 // CODELAB: Precache static resources here.
 evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -12,7 +38,10 @@ evt.waitUntil(
       return cache.addAll(FILES_TO_CACHE);
     })
 );
+});
 
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
 // CODELAB: Remove previous cached data from disk.
 evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -24,8 +53,11 @@ evt.waitUntil(
       }));
     })
 );
+});
 
-// CODELAB: Add fetch event handler here.
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+ // CODELAB: Add fetch event handler here.
 if (evt.request.mode !== 'navigate') {
   // Not a page navigation, bail.
   return;
@@ -39,3 +71,4 @@ evt.respondWith(
               });
         })
 );
+});
